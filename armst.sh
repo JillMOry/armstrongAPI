c21ede1d3148a00058b9a931d96a54f49215e0da
JillMOry <oryjill@gmail.com>
2021-12-29 22:20:42 -0600

    added yaml file for do button

--- armst.sh
+++ armst.sh
@@ -2,9 +2,10 @@
 
 headers=$1
 id=$2
-url=$(curl ${headers} 'https://armstrong-api-75xtt.ondigitalocean.app/'${id})
+# url=$(curl ${headers} 'https://armstrong-api-75xtt.ondigitalocean.app/'${id})
+curl ${headers} 'https://armstrong-api-75xtt.ondigitalocean.app/'${id}
 
-echo ${url}
+# echo ${url}
 
 
 
